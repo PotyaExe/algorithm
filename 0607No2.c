a87ed663376ff5fe3c6b3caa817e27014f39ce9d
potyama <kamikaze11717@gmail.com>
2018-06-08 19:46:58 +0900

    H29sample.pdfより

---
+++ 0607No2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct CELL {
+	char ch;
+	int mask;
+	struct CELL *next;
+} CELL;
+
+void print_list(CELL *root);
+
+int main(void)
+{
+	CELL root;
+	CELL *p;
+	int i;
+
+	char word[50] = "very easy";
+
+	srand((unsigned)time(NULL));
+
+	root.next = NULL;
+	p = &root;
+
+	for  ( i = 0; word[i]!='\0'; i++ ) {
+		p->next = (CELL *)malloc(sizeof(CELL));
+		p = p->next;
+		p->ch = word[i];
+		p->mask = rand()%2;
+	}
+	p->next = NULL;
+
+	print_list(root.next);
+
+	return 0;
+}
+
+void print_list(CELL *root)
+{
+	CELL *p;
+
+	for (p = root; p != NULL; p = p->next) {
+		if(p->mask==0){
+            printf("%c", p->ch);
+        }else{
+            printf("-");
+        }
+    }
+	printf("\n");
+    for(p = root;p!=NULL;p = p->next){
+        printf("%d",p->mask);
+    }
+    printf("\n");
+}
+
+
