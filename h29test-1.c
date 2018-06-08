6841a98684185a30ee9a2e59a006af9a0578eef9
potyama <kamikaze11717@gmail.com>
2018-06-08 19:14:00 +0900

    delete

--- h29test-1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-    int i=5;
-    printf("%d\n",i);//5
-    return 0;
-}
--- h29test-2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int i=5;
-    int *ip = &i;
-    printf("%d\n",*ip);//5
-}
